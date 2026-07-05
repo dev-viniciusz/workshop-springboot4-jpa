bd93fdca543a3a0b23017034a911ade4e29a75b1
Vinicius Nunes <viniciusnunes1002@gmail.com>
2026-07-04 22:47:49 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/web_services/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.web_services.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.web_services.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
