96af915b8ae3ba97fcbc236dfcfec925a0de7260
Vinicius Nunes <viniciusnunes1002@gmail.com>
2026-07-05 16:54:24 -0300

    Service layer, component registration

--- src/main/java/com/web_services/repositories/UserRepository.java
+++ src/main/java/com/web_services/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web_services.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
